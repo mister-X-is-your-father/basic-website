d585cd7bc5fac7eca0f22c487434cb08a4770bc9
HiroshiMorita <ikimonogakaru@gmail.com>
2019-10-13 21:05:50 +0900

    compiling-assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>website</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+  @include('inc.navbar')
   @yield('content')
-  <!-- @includeはsidebarの親の@section~@showまでがある -->
+  {{--コメント-> @includeはsidebarの親の@section~@showまでがある --}}
   @include('inc.sidebar')
 </body>
 </html>
