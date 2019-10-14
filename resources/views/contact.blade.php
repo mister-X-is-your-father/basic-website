1f7fe6c4f693fe0289ec4b08cd104a4fc226d999
HiroshiMorita <ikimonogakaru@gmail.com>
2019-10-14 12:04:02 +0900

    create form view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,18 +5,26 @@
 
 <form>
   <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" name="name" id="name" placeholder="Enter name">
+  </div>
+
+  <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
+
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="subject">Subject</label>
+    <input type="text" class="form-control" name="subject" id="subject" placeholder="Enter subject">
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+  <div class="form-group">
+    <label for="message">Message</label>
+    <textarea class="form-control" name="message" id="message" rows="3"></textarea>
   </div>
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
